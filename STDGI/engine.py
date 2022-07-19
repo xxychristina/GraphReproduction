035a067decf4367daac956a50a704c54389c8487
Christina Xie <xxychristina@gmail.com>
2022-07-19 12:41:29 +1000

    first version STDGI done

--- STDGI/engine.py
+++ STDGI/engine.py
@@ -2,6 +2,7 @@ from models import STDGI
 import torch.optim as optim
 import torch.nn as nn
 import torch
+import util
 
 
 class trainer():
@@ -10,30 +11,74 @@ class trainer():
     self.model = STDGI(ft_size, hid_units)
     self.adj = adj
     self.lr = lr
-    self.b_xent = nn.BCEWithLogitsLoss()
+    self.b_xent = nn.BCELoss()
+    self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr) #first 20
+    self.optimizer1 = optim.Adam(self.model.parameters(), lr=self.lr * 0.9) #first 50
+    self.optimizer2 = optim.Adam(self.model.parameters(), lr=self.lr * 0.8) #first 80
+    self.optimizer3 = optim.Adam(self.model.parameters(), lr=self.lr * 0.7) #first 100
 
-  
-  def train(self, intput):
-    self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
-    self.optimizer.zero_grad()
+
+  def train(self, input, epoch):
+    optimizer = None
+    if epoch < 20:
+      optimizer = self.optimizer
+    elif epoch > 20 and epoch < 50:
+      optimizer = self.optimizer1
+    elif epoch < 80:
+      optimizer = self.optimizer2
+    else:
+      optimizer = self.optimizer3
+
+    optimizer.zero_grad()
 
     self.model.train()
-    loss, embed = self.model(intput)
+    # input = nn.functional.pad(input,(1,0,0,0))
+    # print(input.shape)
+    # shape: [64, 12, 207, 2]
+    logits = self.model(input, self.adj)
 
-    loss = loss/11
+    # loss = loss/11
 
-    batch_size = len(input[0])
-    nb_nodes = len(input[1])
+    # batch_size = len(input[0])
+    # nb_nodes = len(input[1])
 
-    lbl_1 = torch.ones(batch_size, nb_nodes)
-    lbl_2 = torch.zeros(batch_size, nb_nodes)
+    lbl_1 = torch.ones(64, 207)
+    lbl_2 = torch.zeros(64, 207)
     lbl = torch.cat((lbl_1, lbl_2), 1)
 
-    loss = self.b_xent(loss, lbl)
-    loss.backward()
-    self.optimizer.step()
+    # print(logits)
+    # print(lbl)
+    loss = self.b_xent(logits, lbl)
+    # print(loss)
 
-    return loss, embed
+    loss.backward()
+    optimizer.step()
+
+    return loss.item()
+
+  def embed(self, seq):
+    h_1 = self.model.embed(seq)
+    return h_1
+
+  def save(self):
+    torch.save(self.model.state_dict(), 'best_dgi.pkl')
+
+  def load(self):
+    return self.model.load_state_dict(torch.load('best_dgi.pkl'))
+
+  def eval(self, input, real_val):
+    self.model.eval()
+    input = nn.functional.pad(input,(1,0,0,0))
+    output = self.model(input)
+    output = output.transpose(1,3)
+    #output = [batch_size,12,num_nodes,1]
+    real = torch.unsqueeze(real_val,dim=1)
+    predict = self.scaler.inverse_transform(output)
+    loss = self.loss(predict, real, 0.0)
+    mape = util.masked_mape(predict,real,0.0).item()
+    rmse = util.masked_rmse(predict,real,0.0).item()
+    return loss.item(),mape,rmse
+
 
 
 
--- STDGI/layers/GCN.py
+++ STDGI/layers/GCN.py
@@ -1,8 +1,76 @@
+
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
+# https://uvadlc-notebooks.readthedocs.io/en/latest/tutorial_notebooks/tutorial7/GNN_overview.html
+# class GCN(nn.Module):
+#     def __init__(self, c_in, c_out):
+#         super(GCN, self).__init__()
+#         self.projection = nn.Linear(c_in, c_out,bias=False)
+
+#     def forward(self, node_feats, adj_matrix):
+#         """
+#         Inputs:
+#             node_feats - Tensor with node features of shape [batch_size, num_nodes, c_in]
+#             adj_matrix - Batch of adjacency matrices of the graph. If there is an edge from i to j, adj_matrix[b,i,j]=1 else 0.
+#                          Supports directed edges by non-symmetric matrices. Assumes to already have added the identity connections.
+#                          Shape: [batch_size, num_nodes, num_nodes]
+#         """
+#         # Num neighbours = number of incoming edges
+#         num_neighbours = adj_matrix.sum(dim=-1, keepdims=True)
+#         node_feats = self.projection(node_feats)
+#         print(node_feats.size())
+#         print(adj_matrix.size())
+#         node_feats = torch.matmul(node_feats, adj_matrix)
+#         node_feats = node_feats / num_neighbours
+#         return node_feats
+
+# # https://github.com/nnzhan/Graph-WaveNet/issues/15
+# class nconv(nn.Module):
+#     def __init__(self):
+#         super(nconv,self).__init__()
+
+#     def forward(self,x, A):
+#         x = torch.einsum('ntv,vw->ntw',(x,A))
+#         return x.contiguous()
+
+# class linear(nn.Module):
+#     def __init__(self,c_in,c_out):
+#         super(linear,self).__init__()
+#         self.mlp = torch.nn.Conv2d(c_in, c_out, kernel_size=(1, 1), padding=(0,0), stride=(1,1), bias=True)
+
+#     def forward(self,x):
+#         return self.mlp(x)
+
+# class GCN(nn.Module):
+#     def __init__(self,c_in, c_out):
+#         super(GCN,self).__init__()
+#         # self.nconv = nconv()
+#         # self.mlp = linear(c_in,c_out)
+#         # self.dropout = dropout
+#         # self.order = order
+#         self.nconv = nconv()
+#         self.fc = nn.Linear(c_in, c_out, bias=False)
+#         # self.act = nn.PReLU()
+
+#     def forward(self,x,support):
+#         out = []
+#         for a in support:
+#             x1 = self.nconv(x,a)
+#             out.append(x1)
+#             # for k in range(2, self.order + 1):
+#             #     x2 = self.nconv(x1,a)
+#             #     out.append(x2)
+#             #     x1 = x2
+
+#         h = torch.cat(out,dim=1)
+#         # h = self.mlp(h)
+#         # h = F.dropout(h, self.dropout, training=self.training)
+#         return h
 
 class GCN(nn.Module):
-    def __init__(self, in_ft, out_ft, act, bias=True):
+    def __init__(self, in_ft, out_ft, act='prelu', bias=True):
         super(GCN, self).__init__()
         self.fc = nn.Linear(in_ft, out_ft, bias=False)
         self.act = nn.PReLU() if act == 'prelu' else act
@@ -31,8 +99,11 @@ class GCN(nn.Module):
             out = torch.unsqueeze(torch.spmm(adj, torch.squeeze(seq_fts, 0)), 0)
         else:
             #bmm = batch matrix-matrix product of matrices
-            out = torch.bmm(adj, seq_fts)
+            out = torch.matmul(adj, seq_fts)
+            out = self.act(out)
+            out = torch.matmul(adj, out)
+            out = self.act(out)
         if self.bias is not None:
             out += self.bias
         
-        return self.act(out)
+        return out
