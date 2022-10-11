71436410180125e7ae80c3094dab13bc7543ae88
Christina Xie <xxychristina@gmail.com>
2022-10-11 15:33:02 +1100

    fix test

--- STGCL/train.py
+++ STGCL/train.py
@@ -90,42 +90,42 @@ for i in range(1, 101):
     print(f'Epoch: {i}, TrainLoss: {np.mean(train_loss)}, ValidLoss: {mvalid_loss}')
     torch.save(engine.model.state_dict(), "_epoch_"+str(i)+"_"+str(round(mvalid_loss,2))+".pth")
 
-    #testing
-    bestid = np.argmin(his_loss)
-    engine.model.load_state_dict(torch.load("_epoch_"+str(bestid+1)+"_"+str(round(his_loss[bestid],2))+".pth"))
+#testing
+bestid = np.argmin(his_loss)
+engine.model.load_state_dict(torch.load("_epoch_"+str(bestid+1)+"_"+str(round(his_loss[bestid],2))+".pth"))
 
-    outputs = []
-    realy = torch.Tensor(dataloader['y_test']).to(device)
-    realy = realy.transpose(1,3)[:,0,:,:]
+outputs = []
+realy = torch.Tensor(dataloader['y_test']).to(device)
+realy = realy.transpose(1,3)[:,0,:,:]
 
-    for iter, (x, y) in enumerate(dataloader['test_loader'].get_iterator()):
-        testx = torch.Tensor(x).to(device)
-        testx = testx.transpose(1,3)
-        with torch.no_grad():
-            preds, _, _ = engine.model(testx)
-            preds = preds.transpose(1,3)
-        outputs.append(preds.squeeze())
+for iter, (x, y) in enumerate(dataloader['test_loader'].get_iterator()):
+    testx = torch.Tensor(x).to(device)
+    testx = testx.transpose(1,3)
+    with torch.no_grad():
+        preds, _, _ = engine.model(testx)
+        preds = preds.transpose(1,3)
+    outputs.append(preds.squeeze())
 
-    yhat = torch.cat(outputs,dim=0)
-    yhat = yhat[:realy.size(0),...]
+yhat = torch.cat(outputs,dim=0)
+yhat = yhat[:realy.size(0),...]
 
-    print("Training finished")
-    print("The valid loss on best model is", str(round(his_loss[bestid],4)))
+print("Training finished")
+print("The valid loss on best model is", str(round(his_loss[bestid],4)))
 
-    amae = []
-    amape = []
-    armse = []
-    for i in range(12):
-        pred = scaler.inverse_transform(yhat[:,:,i])
-        real = realy[:,:,i]
-        metrics = util.metric(pred,real)
-        log = 'Evaluate best model on test data for horizon {:d}, Test MAE: {:.4f}, Test MAPE: {:.4f}, Test RMSE: {:.4f}'
-        print(log.format(i+1, metrics[0], metrics[1], metrics[2]))
-        amae.append(metrics[0])
-        amape.append(metrics[1])
-        armse.append(metrics[2])
+amae = []
+amape = []
+armse = []
+for i in range(12):
+    pred = scaler.inverse_transform(yhat[:,:,i])
+    real = realy[:,:,i]
+    metrics = util.metric(pred,real)
+    log = 'Evaluate best model on test data for horizon {:d}, Test MAE: {:.4f}, Test MAPE: {:.4f}, Test RMSE: {:.4f}'
+    print(log.format(i+1, metrics[0], metrics[1], metrics[2]))
+    amae.append(metrics[0])
+    amape.append(metrics[1])
+    armse.append(metrics[2])
 
-    log = 'On average over 12 horizons, Test MAE: {:.4f}, Test MAPE: {:.4f}, Test RMSE: {:.4f}'
-    print(log.format(np.mean(amae),np.mean(amape),np.mean(armse)))
-    torch.save(engine.model.state_dict(), "_exp"+str(1)+"_best_"+str(round(his_loss[bestid],2))+".pth")
-    
+log = 'On average over 12 horizons, Test MAE: {:.4f}, Test MAPE: {:.4f}, Test RMSE: {:.4f}'
+print(log.format(np.mean(amae),np.mean(amape),np.mean(armse)))
+torch.save(engine.model.state_dict(), "_exp"+str(1)+"_best_"+str(round(his_loss[bestid],2))+".pth")
+
