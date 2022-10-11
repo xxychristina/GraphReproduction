d6baaa7332818f9665581e15bd4243a2e556a81c
Christina Xie <xxychristina@gmail.com>
2022-10-11 12:33:40 +1100

    c_rate = 0.1

--- STGCL/train.py
+++ STGCL/train.py
@@ -34,13 +34,14 @@ seq_length= 12
 nhid = 32
 scaler = dataloader['scaler']
 lr = 0.001
+c_rate = 0.1
 
 projection_head = nn.Sequential(nn.Linear(256, 256), nn.ReLU(inplace=True), nn.Linear(256, 256))
 encoder = gwnetEncoder(device, num_nodes, dropout, supports=supports, gcn_bool=gcn_bool, addaptadj=addaptadj, in_dim=in_dim, out_dim=seq_length, residual_channels=nhid, dilation_channels=nhid, skip_channels=nhid * 8, end_channels=nhid * 16)
 decoder = gwnetDecoder(out_dim=seq_length,skip_channels=nhid * 8,end_channels=nhid * 16)
 
 from engine import trainer
-engine = trainer(scaler, device, adj_mx, lr, encoder, decoder)
+engine = trainer(scaler, device, adj_mx, lr, encoder, decoder, c_rate=c_rate)
 his_loss =[]
 
 for i in range(1, 101):
